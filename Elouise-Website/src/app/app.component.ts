b8c363ea371be3e57f74a22c5ec3f3907e0f2190
Bradley Warner <bradley@freelancecomputing.net>
2017-05-05 02:39:43 +1000

    - Nearly finished tutorial

--- Elouise-Website/src/app/app.component.ts
+++ Elouise-Website/src/app/app.component.ts
@@ -1,18 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -74,12 +62,21 @@ const HEROES: Hero[] = [
 			border-radius: 4px 0 0 4px;
 		}
 	`],
+	providers: [ HeroService ],
 })
-export class AppComponent {
-	heroes = HEROES;
+
+export class AppComponent implements OnInit {
+	constructor(private heroService: HeroService) {}
+	heroes : Hero[];
 	selectedHero: Hero;
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+	getHeroes(): void {
+		this.heroService.getHeroes().then( heroes => this.heroes = heroes );
+	}
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 }
 
